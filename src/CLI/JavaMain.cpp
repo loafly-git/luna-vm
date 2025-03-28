1722c102373beeee8f292d4d26e95060d4f03c33
irice7 <iricetoo.0@gmail.com>
2025-03-28 23:36:32 +0700

    Update JavaMain.cpp

--- src/CLI/JavaMain.cpp
+++ src/CLI/JavaMain.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <string>
 
-#include "JavaArgumentParser.h"
-
-using namespace Luna;
-
 int main(int argc, char* argv[])
 {
-
     if(argc <= 1)
     {
         std::cerr << "No arguments specified." << std::endl;
