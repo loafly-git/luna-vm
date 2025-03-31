0dbea80795ea3cc18f83b672570b856b34865720
irice7 <iricetoo.0@gmail.com>
2025-03-31 12:00:09 +0700

    Small changes; ignore.

--- src/CLI/JavaMain.cpp
+++ src/CLI/JavaMain.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "Log/Log.h"
+
 int main(int argc, char* argv[])
 {
     if(argc <= 1)
@@ -8,6 +10,8 @@ int main(int argc, char* argv[])
         std::cerr << "No arguments specified." << std::endl;
         return 1;
     }
+
+    LNLOGD("The meaning of life is {}", 42);
 
     return 0;
 }
--- src/Log/Log.h
+++ src/Log/Log.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <format>
+#include <string>
 #include <string_view>
 #include <utility>
 
